254cef9ee65c81e1f60527b522a071efcd0b3259
Nicole Dreon <dreon.nicole@gmail.com>
2021-03-09 16:42:48 +0000

    add 'test.txt' to 'wordcount.py' and create empty dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,13 @@
 # put your code here.
+
+# Write a program, wordcount.py, that opens a file and counts how many times each space-separated word occurs in that file. Your program should then print those counts to the screen.
+
+# open file save as variable
+# create empty dictionary save as variable
+# loop through each line
+# def - divide line by space .rstrip()-remove white space save list for each line 
+# def - get count of words 
+# put list of words and count into bucket
+
+file = open('test.txt')
+word_count = {}
